ff87f43ecce166cd7df9f18aa06afefe1f7cab22
Carlos Henrique <cid360@hotmail.com>
2020-06-23 10:23:17 -0300

    gh-pages update

---
+++ react/precache-manifest.997bea0dfb0a5accfa3a3af853b7f335.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7302c28714acb1cb71762cbd12512fa1",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.c5b622746266f6981f16.bundle.js"
+  },
+  {
+    "url": "runtime~main.c5b622746266f6981f16.bundle.js"
+  },
+  {
+    "revision": "11f27242420c268b9b87",
+    "url": "static/css/main.d5374cbf.chunk.css"
+  },
+  {
+    "url": "vendors~main.c5b622746266f6981f16.bundle.js"
+  },
+  {
+    "url": "vendors~main.c5b622746266f6981f16.bundle.js.LICENSE.txt"
+  }
+]);
